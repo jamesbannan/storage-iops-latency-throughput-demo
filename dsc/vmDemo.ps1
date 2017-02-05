2a6d089d8d25b614e5ab1c7a3e6a85b46f9cacc2
James Bannan <james@bannan.com.au>
2017-02-05 23:39:56 +1100

    Created vmDemo DSC configuration

---
+++ dsc/vmDemo.ps1
@@ -0,0 +1,81 @@
+Configuration vmDemo
+{
+
+Param(
+	[string]$NodeName = 'localhost'
+)
+
+Import-DscResource -Name cChoco,xStorage
+
+Node $NodeName 
+	{
+		LocalConfigurationManager
+			{
+				DebugMode = 'ForceModuleImport'
+
+			}
+		cChocoInstaller installChoco
+			{
+				InstallDir = "c:\choco"
+			}
+		cChocoPackageInstaller installIometer
+			{
+				Name        = "iometer"
+				DependsOn   = "[cChocoInstaller]installChoco"
+				AutoUpgrade = $True
+			}
+		xWaitforDisk Disk2
+			{
+				DiskNumber = 2
+				RetryIntervalSec = 60
+				RetryCount = 60
+			} 
+		xWaitforDisk Disk3
+			{
+				DiskNumber = 3
+				RetryIntervalSec = 60
+				RetryCount = 60
+			} 
+		xWaitforDisk Disk4
+			{
+				DiskNumber = 4
+				RetryIntervalSec = 60
+				RetryCount = 60
+			} 
+		xWaitforDisk Disk5
+			{
+				DiskNumber = 5
+				RetryIntervalSec = 60
+				RetryCount = 60
+			} 
+		xDisk MVolume
+			{
+				DiskNumber = 2
+				DriveLetter = 'M'
+				FSLabel = 'NoCache'
+				FSFormat = 'NTFS'
+			}
+		xDisk NVolume
+			{
+				DiskNumber = 3
+				DriveLetter = 'N'
+				FSLabel = 'ReadCache'
+				FSFormat = 'NTFS'
+			}
+		xDisk OVolume
+			{
+				DiskNumber = 4
+				DriveLetter = 'O'
+				FSLabel = 'RWCache'
+				FSFormat = 'NTFS'
+			}
+		xDisk PVolume
+			{
+				DiskNumber = 5
+				DriveLetter = 'P'
+				FSLabel = 'SSD'
+				FSFormat = 'NTFS'
+			}
+	}
+
+}
